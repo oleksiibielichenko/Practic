f66512239798fe946934ffa8217980252cd41b11
oleksiibielichenko <byelichenko@gmail.com>
2023-04-22 18:19:57 +0300

    sds

---
+++ alona.py
@@ -0,0 +1,12 @@
+def sum(a, b):
+    return a + b
+
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+c = sum(num1, num2)
+print(c)
+
+print(type(c))
+print(type(c))
